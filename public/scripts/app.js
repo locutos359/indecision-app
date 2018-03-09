7504de25195df6502b4dca20e8e11727463e6082
Matthew Richman <mrichman@zulily.com>
2018-03-09 15:24:35 -0800

    Added some logical operators

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,15 @@
 
 console.log('App.js is running');
 
-// JSX - JavaScript XML
+// Simple object
 var app = {
-    title: 'IndecisionApp',
-    subtitle: 'This is going to be cool...someday'
+    title: 'Indecision App',
+    subtitle: 'This is going to be cool...someday',
+    options: ['One', 'Two']
 };
 
+// JSX - JavaScript XML
+// Note the use of braces to use JS code
 var template = React.createElement(
     'div',
     null,
@@ -16,10 +19,57 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+    )
+);
+
+/**
+ * @param {string} location
+ * @return {string} locationHtml
+ */
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var user = {
+    name: 'Matt',
+    age: 28,
+    location: 'Seattle'
+};
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        getLocation(user.location)
     )
 );
 
--- src/app.js
+++ src/app.js
@@ -1,15 +1,43 @@
 console.log('App.js is running');
 
-// JSX - JavaScript XML
+// Simple object
 const app = {
-    title: 'IndecisionApp',
+    title: 'Indecision App',
     subtitle: 'This is going to be cool...someday',
+    options: ['One', 'Two'],
 };
 
+// JSX - JavaScript XML
+// Note the use of braces to use JS code
 const template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    </div>
+);
+
+/**
+ * @param {string} location
+ * @return {string} locationHtml
+ */
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
+const user = {
+    name: 'Matt',
+    age: 28,
+    location: 'Seattle',
+};
+
+const template2 = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        <p>{getLocation(user.location)}</p>
     </div>
 );
 
