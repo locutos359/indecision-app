88f6477bfe1d8d9357add8bfbe3587788adfb079
Matthew Richman <mrichman@zulily.com>
2018-03-09 14:34:01 -0800

    Added simple object for app properties

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,25 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
+var app = {
+    title: 'IndecisionApp',
+    subtitle: 'This is going to be cool...someday'
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Does this change for realsies'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    )
 );
+
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
-const template = <h1>Does this change for realsies</h1>;
+const app = {
+    title: 'IndecisionApp',
+    subtitle: 'This is going to be cool...someday',
+};
+
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+    </div>
+);
+
 const appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
